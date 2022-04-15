b164dcca1fb9dd33768830fe9215e260b978e112
Grace Tveit <grace.tveit@gmail.com>
2022-04-15 09:23:57 -0700

    feat: adding Actix hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpServer, App, web, HttpRequest, HttpResponse, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().json("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
