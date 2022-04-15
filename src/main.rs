6a3545cfdfe0780274f408bd5d84f240c331c1a5
Grace Tveit <grace.tveit@gmail.com>
2022-04-15 13:26:45 -0700

    feat: updating main to use proper routes/handlers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use actix_web::{ HttpServer, App, web, HttpResponse, Responder};
+// extern crate serde;
+// extern crate serde_json;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod handlers;
+pub mod schema;
+pub mod db_connection;
+pub mod models;
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().json("Hello world!")
@@ -6,7 +17,7 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+    HttpServer::new(|| App::new().route("/", web::get().to(index)).service(handlers::products::index))
     .bind("127.0.0.1:8080")?
     .run()
     .await
